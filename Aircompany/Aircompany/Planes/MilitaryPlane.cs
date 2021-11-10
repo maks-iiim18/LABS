45312c87115f844e057ac4123d5d05302dc2a9cb
maks.iiim <dziadziukmaksim@gmail.com>
2021-11-10 11:54:28 +0300

    Lab_7 for Linux

---
+++ Aircompany/Aircompany/Planes/MilitaryPlane.cs
@@ -0,0 +1,42 @@
+﻿using Aircompany.Models;
+
+namespace Aircompany.Planes
+{
+    public class MilitaryPlane : Plane
+    {
+        private readonly MilitaryType MilitaryType;
+
+        public MilitaryPlane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, MilitaryType militaryType)
+            : base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
+        {
+            MilitaryType = militaryType;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is MilitaryPlane plane &&
+                   base.Equals(obj) &&
+                   MilitaryType == plane.MilitaryType;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1701194404;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
+            hashCode = hashCode * -1521134295 + MilitaryType.GetHashCode();
+            return hashCode;
+        }
+
+        public MilitaryType GetPlaneType()
+        {
+            return MilitaryType;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString().Replace("}",
+                    ", type=" + MilitaryType +
+                    '}');
+        }
+    }
+}
---
+++ Aircompany/AircompanyTests/Tests/AirportTest.cs
@@ -0,0 +1,65 @@
+using Aircompany;
+using Aircompany.Models;
+using Aircompany.Planes;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AircompanyTests.Tests
+{
+    [TestFixture]
+    public class AirportTest
+    {
+        private Airport Airport;
+        private List<Plane> Planes;
+        [SetUp]
+        public void TestDataSetUp()
+        {
+
+            Planes = new List<Plane>()
+            {
+                new PassengerPlane("Boeing-737", 900, 12000, 60500, 164),
+                new PassengerPlane("Boeing-737-800", 940, 12300, 63870, 192),
+                new PassengerPlane("Boeing-747", 980, 16100, 70500, 242),
+                new PassengerPlane("Airbus A320", 930, 11800, 65500, 188),
+                new PassengerPlane("Airbus A330", 990, 14800, 80500, 222),
+                new PassengerPlane("Embraer 190", 870, 8100, 30800, 64),
+                new PassengerPlane("Sukhoi Superjet 100", 870, 11500, 50500, 140),
+                new PassengerPlane("Bombardier CS300", 920, 11000, 60700, 196),
+                new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.BOMBER),
+                new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryType.BOMBER),
+                new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.BOMBER),
+                new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.FIGHTER),
+                new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.FIGHTER),
+                new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT)
+            };
+            Airport = new Airport(Planes);
+        }
+
+        [Test]
+        public void GetMilitaryPlanes_HasAnyTransportMilitaryPlanes_ReturnsTrue()
+        {
+            bool hasAnyTransportMilitaryPlanes = Airport.GetTransportMilitaryPlanes().Any(plane => plane.GetPlaneType() == MilitaryType.TRANSPORT);
+
+            Assert.True(hasAnyTransportMilitaryPlanes);
+        }
+
+        [Test]
+        public void GetPassengerPlaneWithMaxPassengerCapacity_ReturnsExpectedPlaneValue()
+        {
+            var expectedPlaneWithMaxPassengerCapacity = new PassengerPlane("Boeing-747", 980, 16100, 70500, 242);
+            var actualPlaneWithMaxPassengerCapacity = Airport.GetPassengerPlaneWithMaxPassengersCapacity();
+
+            Assert.AreEqual(expectedPlaneWithMaxPassengerCapacity, actualPlaneWithMaxPassengerCapacity);
+        }
+
+        [Test]
+        public void SortPlanesByMaxLoadCapacity_ReturnExpectedPlaneList()
+        {
+            var expectedPlaneList = Airport.GetPlanes<Plane>().OrderBy(plane => plane.GetMaxLoadCapacity());
+            var actualPlaneList = Airport.SortByMaxLoadCapacity().GetPlanes<Plane>();
+
+            CollectionAssert.AreEqual(expectedPlaneList, actualPlaneList);
+        }
+    }
+}
