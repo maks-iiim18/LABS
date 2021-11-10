40367e76d00c2e786661af7987da54e94388a87e
maks.iiim <dziadziukmaksim@gmail.com>
2021-11-10 11:34:01 +0300

    Lab_7

---
+++ Net/Aircompany/Airport.cs
@@ -0,0 +1,55 @@
+﻿using Aircompany.Models;
+using Aircompany.Planes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aircompany
+{
+    public class Airport
+    {
+        private readonly IEnumerable<Plane> Planes;
+
+        public Airport(IEnumerable<Plane> planes)
+        {
+            Planes = planes;
+        }
+
+        public IEnumerable<T> GetPlanes<T>() where T : Plane
+        {
+            return Planes.Where(plane => plane.GetType() == typeof(T)).Cast<T>();
+        }
+
+        public PassengerPlane GetPassengerPlaneWithMaxPassengersCapacity()
+        {
+            return GetPlanes<PassengerPlane>().OrderByDescending(plane => plane.GetPassengersCapacity()).First();
+        }
+
+        public IEnumerable<MilitaryPlane> GetTransportMilitaryPlanes()
+        {
+            return GetPlanes<MilitaryPlane>().Where(plane => plane.GetPlaneType() == MilitaryType.TRANSPORT);
+        }
+
+        public Airport SortByMaxDistance()
+        {
+            return new Airport(Planes.OrderBy(w => w.GetMaxFlightDistance()));
+        }
+
+        public Airport SortByMaxSpeed()
+        {
+            return new Airport(Planes.OrderBy(w => w.GetMaxSpeed()));
+        }
+
+        public Airport SortByMaxLoadCapacity()
+        {
+            return new Airport(Planes.OrderBy(w => w.GetMaxLoadCapacity()));
+        }
+
+        public override string ToString()
+        {
+            return "Airport{" +
+                    "planes=" + string.Join(", ", Planes.Select(x => x.GetModel())) +
+                    '}';
+        }
+    }
+}
---
+++ Net/Aircompany/Planes/PassengerPlane.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Aircompany.Planes
+{
+    public class PassengerPlane : Plane
+    {
+        private readonly int PassengersCapacity;
+
+        public PassengerPlane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, int passengersCapacity)
+            : base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
+        {
+            PassengersCapacity = passengersCapacity;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is PassengerPlane plane &&
+                   base.Equals(obj) &&
+                   PassengersCapacity == plane.PassengersCapacity;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 751774561;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
+            hashCode = hashCode * -1521134295 + PassengersCapacity.GetHashCode();
+            return hashCode;
+        }
+
+        public int GetPassengersCapacity()
+        {
+            return PassengersCapacity;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString().Replace("}",
+                    ", passengersCapacity=" + PassengersCapacity +
+                    '}');
+        }
+    }
+}
